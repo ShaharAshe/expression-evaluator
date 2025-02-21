f3028294a8e5935cc977a1374dd751252c28c659
ShaharAshe <shaharas30@gmail.com>
2025-02-21 13:42:53 +0200

    Change expressionIndex variable to local//

--- src/main/java/shaharas/Controller.java
+++ src/main/java/shaharas/Controller.java
@@ -8,17 +8,16 @@ import java.util.HashMap;
 
 public class Controller {
     private final String fileName;
-    private HashMap<String, Operators> operatorsFactory; /* TODO: Implement the factory */
     private final ArrayList<Expression> expressionsInput; // List of expressions read from the file/ console
     private HashMap<String, VariableEXP> variables; // List of variables and their values
-    private int expressionIndex;
+    private HashMap<String, Operators> operatorsFactory; /* TODO: Implement the factory */
 
     Controller(String fileName) {
         this.expressionsInput = new ArrayList<>();
         this.variables = new HashMap<>();
-        this.operatorsFactory = new HashMap<>(); /* TODO: Implement the factory */
-
         this.fileName = fileName;
+
+        this.operatorsFactory = new HashMap<>(); /* TODO: Implement the factory */
     }
 
     public void readInput() throws FileNotFoundException {
@@ -38,18 +37,18 @@ public class Controller {
     public void calculate(){
         /* TODO: Implement the calculate method */
         for (Expression expression : this.expressionsInput) {
-            this.expressionIndex = 0; // Reset the index for each expression
+            int expressionIndex = 0; // Reset the index for each expression
 
             /* Save the variable from the expression */
-            String variable = expression.getExpressionByIndex(this.expressionIndex++);
-            this.expressionIndex++; // Skip the space
+            String variable = expression.getExpressionByIndex(expressionIndex++);
+            expressionIndex++; // Skip the space
 
             /* TODO: Search for the equal sign and skip it */
 
             /* FIXME: Print the variable and the expression/ need to do the calculation */
             /* -- */
             System.out.println(variable + ": ");
-            for(int i = this.expressionIndex; i < expression.size(); i++) {
+            for(int i = expressionIndex; i < expression.size(); i++) {
                 System.out.print(expression.getExpressionByIndex(i));
             }
             System.out.println("\n");
